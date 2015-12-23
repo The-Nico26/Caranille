bd68403571e3a67ad7b59d0128d1f7159bea1523
Caranille <mycaranille@gmail.com>
2015-12-23 08:28:53 +0100

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -13,22 +13,50 @@ hasAdmin($accountAccess); ?>
 <div class="panel panel-warning">
 	<div class="panel-heading"></div>
 	<div class="panel-body">
-		<div class="important"><?= $aindex0 ?></div><br>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root'].'/Admin/Accounts/index.php'; ?>"><?= $aindex1 ?></a>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root'].'/Admin/Configuration/index.php'; ?>"><?= $aindex2 ?></a><br><br>
-		<div class="important"><?= $aindex3 ?></div><br>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root']."/Admin/Story/index.php"; ?>"><?= $aindex4 ?></a>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root']."/Admin/Invocations/index.php"; ?>"><?= $aindex5 ?></a>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root']."/Admin/Equipments/index.php"; ?>"><?= $aindex6 ?></a>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root'].'/Admin/Magics/index.php'; ?>"><?= $aindex7 ?></a>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root'].'/Admin/Missions/index.php'; ?>"><?= $aindex8 ?></a>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root'].'/Admin/Monsters/index.php'; ?>"><?= $aindex9 ?></a>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root'].'/Admin/News/index.php'; ?>"><?= $aindex10 ?></a>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root'].'/Admin/Items/index.php'; ?>"><?= $aindex11 ?></a>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root'].'/Admin/Parchments/index.php'; ?>"><?= $aindex12 ?></a>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root'].'/Admin/Towns/index.php'; ?>"><?= $aindex13 ?></a><br><br>
-		<div class="important"><?= $aindex14 ?></div><br>
-		<a class="btn btn-info" href="<?php echo $_SESSION['Link_Root'].'/Admin/Modules/Design.php'; ?>"><?= $aindex15 ?></a>
+		<div class="row" style="text-align:center;">
+			<div class="col-lg-4">
+			<div class="important"><?= $aindex0 ?></div><br>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root'].'/Admin/Accounts/index.php'; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex1 ?>">
+				</form>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root'].'/Admin/Configuration/index.php'; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex2 ?>">
+				</form>
+			</div>
+			<div class="col-lg-4">
+			<div class="important"><?= $aindex3 ?></div><br>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root']."/Admin/Story/index.php"; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex4 ?>">
+				</form>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root'].'/Admin/Invocations/index.php'; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex5 ?>">
+				</form>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root'].'/Admin/Equipments/index.php'; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex6 ?>">
+				</form>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root'].'/Admin/Magics/index.php'; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex7 ?>">
+				</form>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root'].'/Admin/Missions/index.php'; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex8 ?>">
+				</form>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root'].'/Admin/Monsters/index.php'; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex9 ?>">
+				</form>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root'].'/Admin/News/index.php'; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex10 ?>">
+				</form>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root'].'/Admin/Items/index.php'; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex11 ?>">
+				</form>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root'].'/Admin/Parchments/index.php'; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex12 ?>">
+				</form>
+				<form method="POST" action="<?php echo $_SESSION['Link_Root'].'/Admin/Towns/index.php'; ?>">
+				<input type="submit" class="btn btn-default form-control" value="<?= $aindex13 ?>">
+				</form>
+			</div>
+		</div>
 	</div>
 </div>
 
